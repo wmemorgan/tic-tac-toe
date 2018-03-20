734d575ea77b4f77dda678256774e450f514431f
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-03-20 13:34:30 +0800

    -Add game reset functinality.

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ const chooseOption1 = () => {
     console.log("Player 1 is:", player1Marker);
     player2Marker = 'O';
     console.log("Player 2 is:", player2Marker);
-    gameMenu.style.display = "none";
     showBoard(square);
   } else {
     question.innerHTML = 'Would you like to be X or O?'
@@ -40,7 +39,6 @@ const chooseOption2 = () => {
     console.log("Player 1 is:", player1Marker);
     player2Marker = 'X';
     console.log("Player 2 is:", player2Marker);
-    gameMenu.style.display = "none";
     showBoard(square);
   } else {
     question.innerHTML = 'Player 1: Would you like to be X or O?'
@@ -57,10 +55,12 @@ const goBack = () => {
   question.innerHTML = defaultQuestion;
   option1.innerHTML = defaultOption1;
   option2.innerHTML = defaultOption2;
-  backButton.style.visibility = 'hidden'; 
+  backButton.style.visibility = 'hidden';
+  hideBoard(square); 
 }
 
 const showBoard = (object) => {
+  gameMenu.style.display = "none";
   player1.style.visibility = 'visible';
   player2.style.visibility = 'visible';
   gameResetBtn.style.visibility = 'visible';
@@ -70,6 +70,18 @@ const showBoard = (object) => {
   }
 }
 
+const hideBoard = (object) => {
+  gameMenu.style.display = "grid";
+  player1.style.visibility = 'hidden';
+  player2.style.visibility = 'hidden';
+  gameResetBtn.style.visibility = 'hidden';
+  for (i = 0; i < object.length; i++) {
+    object[i].classList.add("square-hidden");
+    // object[i].innerHTML = i + 1;
+  }
+}
+
 option1.addEventListener("click", chooseOption1);
 option2.addEventListener("click", chooseOption2);
 backButton.addEventListener("click", goBack);
+gameResetBtn.addEventListener("click", goBack);
